0c7f8ce4ac21596c8cf64139883a5489f09a295a
Freddy Gonzalez <freddy.gonzalez@huskers.unl.edu>
2024-07-04 12:01:05 -0400

    update prokka roary pipeline to contain both TB and SP workflows

---
+++ pipelines/Fig2/prokka_roary/scripts/get_SP_paths.sh
@@ -0,0 +1,20 @@
+# This will create a CSV file with the paths to the SP GPSC fasta files
+
+#!/bin/bash
+
+# Directory where GPSCs are stored
+GPSC_dir="/vast/palmer/scratch/turner/flg9/snakemake_workflows/pangenome_alignment/pipelines/SP/prokka_roary/GPSCs/"
+
+# Output CSV file
+output_file="SP_seqs.tsv"
+
+# Write the header line
+echo -e "sample_id\tseq_path" > $output_file
+
+# Loop over the fasta files in GPSCs dir
+for fasta_file in $GPSC_dir*.fasta; do
+    # Extract the sample_id from the file name
+    sample_id=$(basename $fasta_file .fasta)
+    # Write a line to the CSV file with the path to the fasta file
+    echo -e "$sample_id\t$fasta_file" >> $output_file
+done

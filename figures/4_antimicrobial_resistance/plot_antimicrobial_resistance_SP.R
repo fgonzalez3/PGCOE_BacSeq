8eb14228fddcc18eadffb457ecff1ec687c74881
Freddy Gonzalez <freddy.gonzalez@huskers.unl.edu>
2024-08-21 14:00:13 -0400

    latest AMR code upload

--- figures/4_antimicrobial_resistance/plot_antimicrobial_resistance_SP.R
+++ figures/4_antimicrobial_resistance/plot_antimicrobial_resistance_SP.R
@@ -6,7 +6,7 @@ library(patchwork)
 library(ggtext)
 
 # read in csv
-amr <- read.csv("combined_amr_results_diffsamples.csv") 
+amr <- read.csv("combined_amr_results_diffsamples.csv")
 
 # pick reps to visualize
 reps <- c("Yale-CS00030", "Yale-CS00032", "Yale-CS00034", "Yale-CS00036", "Yale-CS00038",
@@ -18,13 +18,49 @@ new_drug_class_names <- c(
   "tetracycline antibiotic" = "Tetracycline", 
   "phosphonic acid antibiotic" = "Fosfomycin", 
   "macrolide antibiotic; lincosamide antibiotic" = "Macrolides and Lincosamides",
-  "macrolide antibiotic; fluoroquinolone antibiotic; aminoglycoside antibiotic; cephalosporin" = "Aminoglycosides",
+  "monobactam; carbapenem; cephalosporin; cephamycin; penam; phenicol antibiotic; penem" = "Beta-lactams and Phenicols",
+  "mupirocin-like antibiotic" = "Mupirocin",
   "glycopeptide antibiotic" = "Glycopeptides", 
-  "macrolide antibiotic" = "Macrolides",
   "fluoroquinolone antibiotic" = "Fluoroquinolone", 
   "cephalosporin; penam; peptide antibiotic" = "Cephalosporins and Penams",
   "carbapenem" = "Carbapenems"
 )
+
+# create a grid of samples and drug classes
+complete_grid <- expand.grid(sample_name = unique(amr$sample_name), 
+                             drug_class = unique(amr$drug_class))
+
+# join grid 
+amr_complete <- complete_grid %>%
+  left_join(amr, by = c("sample_name", "drug_class"))
+
+# make new colname for resistance status
+amr_complete <- amr_complete %>%
+  mutate(resistance_status = ifelse(is.na(resistance_mechanism), "Susceptible", "Resistant"))
+
+# filter out repetitive resistant markers 
+amr_filtered <- amr_complete %>%
+  filter(drug_class != "macrolide antibiotic") %>%
+  filter(drug_class != "macrolide antibiotic; fluoroquinolone antibiotic; monobactam; aminoglycoside antibiotic; carbapenem; cephalosporin; cephamycin; penam; tetracycline antibiotic; phenicol antibiotic; penem; disinfecting agents and antiseptics")
+
+# plot
+amr_plot <- amr_filtered %>%
+  filter(sample_name %in% reps) %>%
+  ggplot(aes(sample_name, drug_class, fill = resistance_status)) + 
+  geom_tile() + 
+  scale_fill_manual(values = c("Resistant" = "purple", "Susceptible" = "lightblue"), 
+                    name = "Resistance Status") + 
+  scale_y_discrete(labels = new_drug_class_names) + 
+  labs(x = "Sample Name", y = "Drug Class") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+amr_plot
+
+ggsave("bacseq_spneumo_czid_amr.png",width=350,height=300,units="mm", bg = "white")
+
+
+############# old code ###########
 
 # plot
 amr_plot <- amr %>%
@@ -43,7 +79,7 @@ amr_plot <- amr %>%
 
 amr_plot
 
-ggsave("bacseq_spneumo_czid_amr.png",width=350,height=300,units="mm", bg = "white")
+
 
 
 
